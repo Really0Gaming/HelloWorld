bec2ce7066d6a95afa93f4c5d5bdfd5ba2b394c0
Really0Gaming <153234895+Really0Gaming@users.noreply.github.com>
2023-12-08 11:09:57 +0100

    Hello

---
+++ program.cs
@@ -0,0 +1,17 @@
+namespace HelloWorld
+{
+    class Hello {         
+            static void Main(string[] args)
+                    {
+                                System.Console.WriteLine("Hello World!");
+                                        }
+                                            }
+                                            }namespace HelloWorld
+                                            {
+                                                class Hello {         
+                                                        static void Main(string[] args)
+                                                                {
+                                                                            System.Console.WriteLine("Hello World!");
+                                                                                    }
+                                                                                        }
+                                                                                        }
